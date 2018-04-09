4b85f65aa4131cb0ece6c3ff1d3fc6051b07b6dd
BenouKat <ben.goze@gmail.com>
2018-04-09 18:29:04 +0200

    Bandana via code re-done, need to be fixed (direction)

--- Assets/Scripts/Utils/BandanaBehaviour.cs
+++ Assets/Scripts/Utils/BandanaBehaviour.cs
@@ -10,15 +10,16 @@ public class BandanaBehaviour : MonoBehaviour {
     public float sectionWidth;
 
     GameObject root;
-    Vector3 oldPosition;
-    Quaternion neutralRotation;
+    Vector2 oldPosition;
+    Vector2 currentPosition;
+    Transform goalRotation;
     public float speedRotationUpward;
     public float speedRotationDownward;
     List<Transform> allJoints = new List<Transform>();
     Dictionary<Transform,Quaternion> jointRotation = new Dictionary<Transform, Quaternion>();
 
     public float maxSpeed;
-    float lastMovementSpeed;
+    float lastMovementSpeed = 0f;
     float newMovementSpeed;
     Transform tempTrans;
     Quaternion tempRot;
@@ -29,46 +30,64 @@ public class BandanaBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        newMovementSpeed = (Vector3.Distance(oldPosition, transform.position) / Time.deltaTime) / maxSpeed;
-            root.transform.rotation = Quaternion.Slerp(transform.rotation,
-            Quaternion.Slerp(neutralRotation, Quaternion.identity, newMovementSpeed),
-            newMovementSpeed >= lastMovementSpeed ? speedRotationUpward : speedRotationDownward);
+        currentPosition.x = transform.position.x;
+        currentPosition.y = transform.position.z;
 
-        for(int i=0; i<allJoints.Count;i++)
+        newMovementSpeed = (Vector2.Distance(oldPosition, currentPosition) / Time.deltaTime) / maxSpeed;
+
+        goalRotation.transform.rotation = Quaternion.identity;
+        goalRotation.LookAt(Vector3.Lerp(goalRotation.transform.position + transform.parent.forward, goalRotation.transform.position - Vector3.up, newMovementSpeed / maxSpeed));
+
+        root.transform.rotation = Quaternion.Slerp(root.transform.rotation, 
+            goalRotation.transform.rotation, 
+            newMovementSpeed > lastMovementSpeed ? speedRotationUpward : speedRotationDownward);
+
+
+        for (int i=0; i<allJoints.Count;i++)
         {
             tempTrans = allJoints[i];
             tempRot = jointRotation[tempTrans];
             tempTrans.rotation = Quaternion.Slerp(tempRot, tempTrans.parent.rotation, lastMovementSpeed >= newMovementSpeed ? speedRotationUpward : speedRotationDownward);
-            jointRotation[tempTrans] = tempRot;
+            jointRotation[tempTrans] = tempTrans.rotation;
         }
-	}
+
+        oldPosition = currentPosition;
+        lastMovementSpeed = newMovementSpeed;
+    }
 
     void GenerateBandana()
     {
         root = new GameObject("Root");
         root.transform.SetParent(transform);
         root.transform.localScale = Vector3.one;
-        root.transform.forward = Vector3.up;
-        oldPosition = root.transform.position;
-        neutralRotation = root.transform.rotation;
+        root.transform.localRotation = Quaternion.identity;
+        root.transform.localPosition = Vector3.zero;
+        oldPosition = new Vector2(root.transform.position.x, root.transform.position.z);
+
+        goalRotation = (new GameObject("RotationHelper")).transform;
+        goalRotation.transform.SetParent(transform);
+        goalRotation.transform.localPosition = Vector3.zero;
+        goalRotation.transform.rotation = root.transform.localRotation;
 
         Transform currentParent = null;
         for(int i=0; i < sectionCount; i++)
         {
             GameObject join = new GameObject("Join");
             join.transform.SetParent(currentParent ?? root.transform);
-            join.transform.localScale = new Vector3(1f, 1f, sectionLength);
+            join.transform.localScale = Vector3.one;
             join.transform.localRotation = Quaternion.identity;
-            if (currentParent != null) join.transform.localPosition = -Vector3.forward;
+            join.transform.localPosition = currentParent != null ? - (Vector3.up*sectionLength*2f) : Vector3.zero;
+            
             currentParent = join.transform;
             allJoints.Add(join.transform);
             jointRotation.Add(join.transform, join.transform.rotation);
 
             GameObject model = Instantiate(quadMesh) as GameObject;
             model.transform.SetParent(join.transform);
-            model.transform.localScale = Vector3.one;
+            model.transform.localScale = new Vector3(1f, 1f, sectionLength);
             model.transform.localRotation = Quaternion.identity;
-            model.transform.localPosition = -Vector3.forward * 0.5f;
+            //model.transform.Rotate(-Vector3.right * 90f);
+            model.transform.localPosition = -Vector3.up * sectionLength;
         }
     }
 }
