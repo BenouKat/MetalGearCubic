b2f3b52abb5f889fc2420b5697ab9443f9696f14
BenouKat <ben.goze@gmail.com>
2018-04-13 17:49:10 +0200

    Add cinemachine, early setup

---
+++ Assets/Scripts/Utils/CameraZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZone : MonoBehaviour
+{
+    public GameObject virtualCamera;
+
+    private void Awake()
+    {
+        if (virtualCamera == null) enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        virtualCamera.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        virtualCamera.SetActive(false);
+    }
+}
